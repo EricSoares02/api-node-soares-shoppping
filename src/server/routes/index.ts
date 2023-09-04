4b4286ceaad4923aa42815587e0a1b9d433c4d82
Eric Soares <ericsoareslopesdev@gmail.com>
2023-09-04 16:28:22 -0300

    feat: create test(post) controller

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,15 +1,6 @@
 import { Router } from "express";
-import { ProdutoController } from "../controllers";
-import { prisma } from "../shared/services/prisma/prisma";
+import { PostsController, ProdutoController } from "../controllers";
 
-export default async function connect() {
-  try {
-    await prisma.$connect();
-    console.log('Database connected sucess')
-  } catch (error) {
-    console.log("Database connected unsucessull");
-  }
-}
 
 export const router = Router();
 
@@ -26,14 +17,4 @@ router.get("/produtos", ProdutoController.getAll);
 router.put("/produtos/:id", ProdutoController.update);
 router.delete("/produtos/:id", ProdutoController.delet);
 router.get("/produtos/:id", ProdutoController.getById);
-router.get("/teste", async (req, res) => {
-  try {
-    connect();
-    const posts = await prisma.post.findMany();
-    return res.json({message:'sucess', posts}).status(200)
-  } catch (error) {
-    res.json({ message: "error", error }).status(500);
-  } finally {
-    await prisma.$disconnect();
-  }
-});
+router.get("/teste", PostsController.getAll);
