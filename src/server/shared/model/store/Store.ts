04f9ea6e1be2bfc494c9a9a66637a2d8ae440dda
Eric Soares <ericsoareslopesdev@gmail.com>
2023-10-30 14:55:42 -0300

    feat: add Store model

--- src/server/shared/model/store/Store.ts
+++ src/server/shared/model/store/Store.ts
@@ -1,11 +1,12 @@
 
 export interface IStore{
 
-    name: string
-    email: string
-    password: string
-    url_img: string
-    cnpj: number
-    desc?: string
+    id: string;
+    name: string;
+    email: string;
+    password: string; 
+    url_img: string;
+    cnpj: number;
+    desc?: string;
 
 }
