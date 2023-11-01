3cb9c14bf1ae41512f5e5bfec512132f44f9862a
Eric Soares <ericsoareslopesdev@gmail.com>
2023-11-01 10:17:22 -0300

    feat: add user model

---
+++ src/server/shared/model/user/User.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+  id: string;
+  first_name: string;
+  last_name: string;
+  url_img?: string;
+  email: string;
+  password: string;
+  //requests: Request[]
+  //comments: Comments[]
+}
