12db7f8c6bb6107f5a0f44bc76aaca4aaff90601
Eric Soares <ericsoareslopesdev@gmail.com>
2023-11-08 19:30:34 -0300

    feat: add get products

--- src/server/shared/services/produtos/getProductById.ts
+++ src/server/shared/services/produtos/getProductById.ts
@@ -6,7 +6,9 @@ export const getProductByIdService = async (id:string | undefined) => {
     connect();
     const product = await prisma.product.findUnique({ where: {id:id} });
     const productStore = await prisma.product.findUnique({ where: {id:id} }).store();
-    return {product, productStore};
+    const productComments = await prisma.comments.findMany({where: {product_commentedId: id }});
+    const T = await prisma.user.findMany({where:{comments:{some:{product_commentedId:id}}}})
+    return {product, productStore, productComments, T};
   } catch (error) {
     console.log(`error:${error}`);
   } finally {
