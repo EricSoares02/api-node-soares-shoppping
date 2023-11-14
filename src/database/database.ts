b46c340f23da2e597631aa501ae4a30d58d9986b
Eric Soares <ericsoareslopesdev@gmail.com>
2023-11-14 15:47:42 -0300

    feat: add diconnect database with prisma

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,7 +1,7 @@
 import { prisma } from "../services/prisma/prisma";
 
 
-export default async function connect() {
+  async function connect() {
     try {
       await prisma.$connect();
       console.log('Database connected sucess')
@@ -9,3 +9,15 @@ export default async function connect() {
       console.log("Database connected unsucessull");
     }
   }
+
+ async function diconnect(){
+  try {
+    await prisma.$disconnect();
+    console.log('Database disconnected sucess')
+  } catch (error) {
+    console.log("Database disconnected unsucessull");
+  }
+}
+
+
+export {connect, diconnect}
