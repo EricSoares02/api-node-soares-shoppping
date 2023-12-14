262043213731cc36aad685d64fb5a145fc9493b6
Eric Soares <ericsoareslopesdev@gmail.com>
2023-12-14 15:47:25 -0300

    feat: add verify user store

--- src/core/product/productCore.ts
+++ src/core/product/productCore.ts
@@ -1,9 +1,17 @@
 import { Product } from "../../interfaces/IProduct";
+import { DecodedTokenJwt } from "../../middleware/decodedToken.Jwt";
 import { ProductRepository } from "../../repositories/product/ProductRepository";
 import { StoreRepository } from "../../repositories/store/CreateStoreRepository";
+import { UserRepository } from "../../repositories/user/UserRepository";
 import { ProductService } from "../../services/product/ProductService";
 import { StoreService } from "../../services/store/createStoreService";
+import { UserService } from "../../services/user/UserService";
 import { VerifyCategory } from "./verifyCategory";
+import Jwt from "jsonwebtoken";
+
+type JwtPayload = {
+  id: string;
+};
 
 class ProductCore {
   public async StoreExist(storeId: string) {
@@ -32,6 +40,31 @@ class ProductCore {
 
     return false;
   }
+
+  //o user que tenta cadastrar o produto, precisa ser da msm loja que está cadastrando o produto, e tbm precisa ser um adm, ou um master ou elder
+  public async verifyUserStore(product: Product, authorization?: string){
+
+   const token = DecodedTokenJwt(authorization);
+   if(token===''){
+    return false
+   }
+   
+  const {id} = Jwt.decode(token) as JwtPayload;
+  const user = await new UserService(new UserRepository()).executeGetByIdUserRepository(id)
+   if(!user){
+    return false
+   }
+
+
+  if((user.storeId !== product.storeId) || user.storeId === undefined){
+    return false
+  }
+
+  return true
+
+  }
+
+
 }
 
 export { ProductCore };
