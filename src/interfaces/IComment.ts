30dda7515fc0248acb4776c3f5a73026775e5fc8
Eric Soares <ericsoareslopesdev@gmail.com>
2024-01-04 08:38:16 -0300

    feat: add comment type get by product

--- src/interfaces/IComment.ts
+++ src/interfaces/IComment.ts
@@ -1,29 +1,32 @@
+export type Comment = {
+  id: string;
+  authorId: string;
+  product_commentedId: string;
+  title: string;
+  stars: number;
+  createdAt: Date;
+};
 
-export type Comment= {
+export interface CommentTypeToGetByProduct {
+    id: string;
+    title: string;
+    stars: number;
+    likes: number
+    createdAt: Date;
+}
+export interface ICommentParams {
+  id: string;
+}
 
-    id: string
-    authorId: string
-    product_commentedId:  string
-    title: string
+export interface ICommentRepositories {
+  create(
+    authorId: string,
+    product_commentedId: string,
+    title: string,
     stars: number
-    
-    }
-    
-    export  interface ICommentParams{
-        id: string
-    }
-    
-    export interface ICommentRepositories {
-        create(
-            authorId: string,
-            product_commentedId:  string,
-            title: string,
-            stars: number
-        ): Promise<Comment>;
-      
-        getByUser(authorId: string): Promise<Comment[]>;
-    
-        getByProduct(product_commentedId: string): Promise<Comment[]>;
-      
-    
-      }
+  ): Promise<Comment>;
+
+  getByUser(authorId: string): Promise<Comment[]>;
+
+  getByProduct(product_commentedId: string): Promise<CommentTypeToGetByProduct[]>;
+}
