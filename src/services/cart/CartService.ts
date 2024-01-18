b08185e8b96ba05b8b1d2a714b07e2a80644946b
Eric Soares <ericsoareslopesdev@gmail.com>
2024-01-18 16:42:07 -0300

    feat: add Cart Service

---
+++ src/services/cart/CartService.ts
@@ -0,0 +1,32 @@
+import { CartRepository } from "../../repositories/cart/CartRepository";
+
+class CartService {
+  constructor(private CartRepository: CartRepository) {}
+
+  public async executeCreateCartRepository(ownerId: string, product_ids: string[]) {
+    const created = await this.CartRepository.create(ownerId, product_ids);
+
+    return created;
+  }
+
+  public async executeGetCartRepository(id: string) {
+    const commentsByProduct = await this.CartRepository.getCart(
+   id
+    );
+
+    return commentsByProduct;
+  }
+
+  public async executeGetProductsByCartRepository(CartId: string) {
+    const commentsByUser = await this.CartRepository.getProductsByCart(CartId);
+
+    return commentsByUser;
+  }
+
+  public async executeInsertProductInCartRepository(id: string, product_ids: string[]) {
+    const created = await this.CartRepository.insertProduct(id, product_ids);
+
+    return created;
+  }
+}
+export { CartService };
