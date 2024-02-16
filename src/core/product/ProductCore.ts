51e28dacd7e6ce1d5f94eeda206b260b3f274533
Eric Soares <ericsoareslopesdev@gmail.com>
2024-02-16 08:59:22 -0300

    feat: add product core

--- src/core/product/ProductCore.ts
+++ src/core/product/ProductCore.ts
@@ -1,24 +1,41 @@
 import { ZodValidationData } from "../../middleware/validationData.Zod";
 import { z } from "zod";
-import { Product, ProductCategoryType } from "../../interfaces/product/product";
+import { IProductParamsToCreate, IProductParamsToUpdate, Product } from "../../interfaces/product/product";
 
 const IdSchema = z.string().length(24);
+const ParamsSchema = z.string().min(1)
+type CategoryTypeToCore = {
+    category: string,
+    subCategory: string
+}
 
 
-const ProductSchema = z.object({
 
-name: z.string().min(3),
-price: z.number().positive(),
-desc: z.string().min(3), 
-photos: z.array(z.string().url()), 
-category: z.object({
+const ProductSchemaToUpdate = z.object({
+    id: IdSchema,
     name: z.string().min(3),
-    subCategory: z.string().min(3)
-})
+    price: z.number().positive(),
+    desc: z.string().min(3), 
+    photos: z.array(z.string().url()), 
+    categoryName: z.string().min(3),
+    subCategoryName: z.string().min(3),
+    storeId: IdSchema
 });
 
 
-const ParamsSchema = z.string().min(2)
+
+
+const ProductSchemaToCreate = z.object({
+    name: z.string().min(3),
+    price: z.number().positive(),
+    desc: z.string().min(3), 
+    photos: z.array(z.string().url()), 
+    categoryName: z.string().min(3),
+    subCategoryName: z.string().min(3),
+    storeId: IdSchema.optional(),
+    });
+
+
 
 
 const OptionsSchema = {
@@ -63,16 +80,20 @@ class ProductCore {
     }
 
 
-    async validationData(data: Product){
-        return await new ZodValidationData(ProductSchema, data).parse();
+    async validationDataToCreate(data: IProductParamsToCreate){
+        return await new ZodValidationData(ProductSchemaToCreate, data).parse();
     }
     
+    async validationDataToUpdate(data: IProductParamsToUpdate){
+        return await new ZodValidationData(ProductSchemaToUpdate, data).parse();
+    }
 
-    async verifyOptions(options: any, category: ProductCategoryType){
+
+    async verifyOptions(options: any, category: CategoryTypeToCore){
     
 
 
-       switch (category.subCategory) {
+       switch (category.category) {
         case 'moda':
         return await new ZodValidationData(OptionsSchema.modaSchema, options).parse();
             
